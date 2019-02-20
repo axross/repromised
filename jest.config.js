c02a074c3b6489e91348118e1bcf4b7634ab52b2
Kohei Asai <me@axross.io>
2019-02-19 19:44:26 -0800

    Update jest.config.js with ts-jest’s modern style (#7)

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,4 @@
 module.exports = {
-  verbose: true,
+  preset: 'ts-jest',
   testEnvironment: 'node',
-  transform: {
-    '^.+\\.tsx?$': 'ts-jest/preprocessor.js',
-  },
-  testRegex: 'test\\.(tsx?)$',
-  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/out/'],
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
 };
